5788b9329c52d53319b517bf858e158aa02521a4
GSALASB <gsalasb7@gmail.com>
2022-03-22 22:07:19 -0600

    Se borro modal.js

--- css/js/modal.js
+++
@@ -1,9 +0,0 @@
-const openEls = document.querySelectorAll("#contenedor-modal");
-const isVisible = "is-visible";
- 
-for(const el of openEls) {
-  el.addEventListener("click", function() {
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-  });
-}
