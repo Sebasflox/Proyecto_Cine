f7d2f2031d872881f1caff24521f2e17b41ad730
GSALASB <gsalasb7@gmail.com>
2022-03-22 22:03:37 -0600

    no se que cambie

---
+++ css/js/modal.js
@@ -0,0 +1,9 @@
+const openEls = document.querySelectorAll("#contenedor-modal");
+const isVisible = "is-visible";
+ 
+for(const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
