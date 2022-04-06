a056328a3b5aaedf1f0440d4bbb561b3c5b21a12
GSALASB <gsalasb7@gmail.com>
2022-04-05 19:48:45 -0600

    index.php

---
+++ shared/footer.php
@@ -0,0 +1,88 @@
+<footer>
+      <div>
+        <div style="width: 100%; text-align: center; border-bottom: solid 2px red; margin-bottom: 13px; position: relative;" >
+          <img id="imgLogo" src="img/Fernando_Logo.png" style="padding-bottom: -50%; position: absolute; bottom: -23px; left: 45%; right: 0;" alt="Logo_inicio">
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+             <div>
+               <p id="texto_footera" href="contactenos.html">Programación</p>
+           </div>
+           </div>
+            <div class="col">
+             <div>
+             <p id="texto_footera" href="contactenos.html">Sobre Fernando</p>
+             </div>
+           </div>
+            <div class="col">
+              <div>
+                 <p id="texto_footera" href="contactenos.html">Publicidad y Eventos</p>
+              </div>
+           </div>
+             <div class="col">
+               <div>
+                 <p id="texto_footera" href="contactenos.html">Contactenos</p>
+               </div>
+             </div>
+          </div>
+        </div>
+       <div class="container" style="background-color: black; margin: 0 !important; width: 100% !important;">
+         <div class="row">
+           <div class="col" style="margin-top: 27px;">
+            <div>
+              <a href="cartelera.html">
+                <p id="texto_footer">Cartelera</p>
+              </a>
+              <a href="proximamente.html">
+                <p id="texto_footer">Próximamente</p>
+              </a>
+              <a href="preventa.html">
+                <p id="texto_footer">Preventa</p>
+              </a>
+          </div>
+          </div>
+           <div class="col " style="margin-top: 27px;">
+            <div>
+            <a href="contactenos.html">
+              <p id="texto_footer">Quienes somos</p>
+            </a>
+            <a href="salas.html">
+              <p id="texto_footer">Salas</p>
+            </a>
+            <a href="contactenos.html">
+              <p id="texto_footer">Terminos y condiciones</p>
+            </a>
+            </div>
+          </div>
+           <div class="col" style="margin-top: 45px;">
+             <div>
+                <a href="contactenos.html">
+                  <p id="texto_footer">Eventos privados</p>
+                </a>
+              <a href="contactenos.html">
+                <p id="texto_footer">Publicidad</p>
+              </a>
+             </div>
+          </div>
+            <div class="col" style="margin-top: 45px;">
+              <div>
+                <a  href="contactenos.html">
+                  <p id="texto_footer">Escribanos</p>
+                </a>
+                <a href="contactenos.html">
+                  <p id="texto_footer">Trabaja con nosotros</p>
+                </a>
+              </div>
+            </div>
+         </div>
+       </div>
+      </div>
+    </footer>
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>  
+<script src="js/index.js"></script>
+</body>
+
+</html>
---
+++ shared/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" style="overflow-x: hidden;">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fernando</title>
+    <link rel="icon" type="img/jfif" href="img/Moneda.jfif">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">   
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script scr="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.bundle.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/estilo_index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
+
+    <script>
+     
+      $(document).on('click', 'btn-modal', function(){
+        $('#modal1').modal('show');
+      });
+    </script>
+
+
+</head>
+
+<!--Botones-->
+<body>
+  
+
+      <header>
+      
+      <div>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end background">
+            <a class="nav-link first_nav" href="servicios.html">Servicios</a>
+            <a class="nav-link first_nav" href="servicios.html">Servicios</a>
+            <a href="registrarse.html"><inpu class="btn btn-primary me-md-2" type="button" value="">Registrarse</a>
+            <a href="IniciarSesion.html"><inpu class="btn btn-primary me-md-2" type="button" value="">IniciarSesion</a>
+            
+        </div>
+        <div class="background2">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="index.html">
+                  <img id="imgLogo" src="img/Fernando_Logo.png" alt="Logo_inicio"> 
+                  </a> 
+                </li>
+                <li class="nav-item">
+                  <a id="nav_text" class="nav-link Texto" href="contactenos.html">Contactenos</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a id="nav_text" class="nav-link dropdown-toggle Texto" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Salas
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="regulares_S.html">Regulares</a></li>
+                    <li><a class="dropdown-item" href="vip_S.html">Vip</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                    <a id="nav_text" class="nav-link Texto" href="protocolos.html">Protocolos</a>
+                  </li>
+              </ul>
+        </div>
+    </div>
+    </header>
