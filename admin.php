6dd8b21f9af950f44de33dcca7beb3f570d6f44c
ElDermans <dermanramirezbenavides@gmail.com>
2022-04-19 17:41:22 -0600

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,14 @@
+<?php include "shared/header.php" ?>
+
+<main>
+
+<a class="btn btn-outline-dark" href="registro_banner.php">registro_banner</a>
+<a class="btn btn-outline-dark" href="registro_horario.php">registro_horario</a>
+<a class="btn btn-outline-dark" href="registro_pelicula.php">registro_pelicula</a>
+<a class="btn btn-outline-dark" href="registro_sala.php">registro_sala</a>
+<a class="btn btn-outline-dark" href="ver_compras.php">ver_compras</a>
+
+</main>
+ 
+
+<?php include "shared/footer.php" ?>
